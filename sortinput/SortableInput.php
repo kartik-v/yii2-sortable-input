4ed6122743f52e2b16aa54d88d3058ff49b1071a
Kartik Visweswaran <kartikv2@gmail.com>
2014-06-26 12:06:30 +0530

    Initial release

--- sortinput/SortableInput.php
+++ sortinput/SortableInput.php
@@ -32,12 +32,13 @@ class SortableInput extends \kartik\widgets\InputWidget
     public $delimiter = ',';
 
     /**
-     * @var array the sortable data list. This allows the following properties to be setup:
-     * - id: int/string, the unique identifier for the item, which will be internally stored. This is mandatory.
-     * - content: string, the content to display for the item (this is not HTML encoded). If this is not set,
-     *   it will default the `id` value.
-     * - disabled: bool, whether the list item is disabled
-     * - options: array, the HTML attributes for the list item.
+     * @var array the sortable data list. You must set it up as `$key => $value` format, where
+     * - key: int/string, the unique identifier (key) for the item, which will be internally stored. This is mandatory.
+     * - value: array, is the configuration for the items array in sortable widget and can consist of the following properties:
+     *   - content: string, the content to display for the item (this is not HTML encoded). If this is not set,
+     *     it will default the `key` setting.
+     *   - disabled: bool, whether the list item is disabled
+     *   - options: array, the HTML attributes for the list item.
      */
     public $items;
 
@@ -73,20 +74,36 @@ class SortableInput extends \kartik\widgets\InputWidget
     protected function initItems()
     {
         $items = [];
-        foreach ($this->items as $item) {
-            if (empty($item['id'])) {
-                throw new InvalidConfigException('The "id" property must be setup for every "item" in the "items" array.');
-            }
+        $this->arrangeItems();
+        foreach ($this->items as $key => $item) {
             if (!isset($item['content'])) {
-                $item['content'] = $item['id'];
+                $item['content'] = $key;
             }
-            $item['options']['data-key'] = $item['id'];
-            unset($item['id']);
+            $item['options']['data-key'] = $key;
             $items[] = $item;
         }
         $this->sortableOptions['items'] = $items;
     }
 
+    /**
+     * Arranges items based on initial value
+     */
+    protected function arrangeItems() 
+    {
+        if (empty($this->value)) {
+            return;
+        }
+        $keys = explode($this->delimiter, $this->value);
+        if (!is_array($keys) || count($keys) == 0) {
+            return;
+        }
+        $items = [];
+        foreach ($keys as $key) {
+            $items[$key] = $this->items[$key];
+        }
+        $this->items = $items;
+    }
+    
     /**
      * Register client assets
      */
